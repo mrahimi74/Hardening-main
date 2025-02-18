7a661d257f918f2ca2cdc4991e644c56191a6741
mrahimi74 <mr-rahimi@outlook.com>
2025-02-17 20:22:22 -0500

    add_func

--- tests/test_predictor_corrector.py
+++ tests/test_predictor_corrector.py
@@ -2,9 +2,16 @@ from PredictorCorrector import predictor_corrector as pc
 import numpy as np
 import pytest
 
-def test_predictor_corrector():
+def test_predictor_corrector_iso():
     E = 1000
     H = 111.1
     Y0 = 10
     material = pc.ElastoPlastic(E, H, Y0)
     assert np.isclose(material.update_step_isotropic(0.0075)[0], 7.5)
+
+def test_predictor_corrector_kin():
+    E = 1000
+    H = 111.1
+    Y0 = 10
+    material = pc.ElastoPlastic(E, H, Y0)
+    assert np.isclose(material.update_step_kinematic(0.0075)[2], -2.5)
